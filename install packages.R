83af2ff9581b6b46fdf9618253a1c3f43d79b76e
Matthias Frye <matthias_frye@hotmail.com>
2023-08-22 21:37:11 +0200

    initial version

---
+++ install packages.R
@@ -0,0 +1,18 @@
+install.packages("tidyverse") 
+install.packages("dslabs")
+install.packages("ggthemes")
+install.packages("ggrepel")
+install.packages("gridExtra")
+install.packages("titanic")
+install.packages("gtools")
+install.packages("markdown")
+
+install.packages("mosaic") # Diesen Befehl bei jeder Session am Anfang ausführen
+install.packages("readr")
+install.packages("gplots")
+install.packages("dunn.test")
+
+install.packages("psych2")
+install.packages("ggplot22")
+install.packages("vcd2")
+

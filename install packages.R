183899367a0b6a322a98ce7b2738a372911205b7
Matthias Frye <matthias_frye@hotmail.com>
2023-08-22 21:40:09 +0200

    commented

--- install packages.R
+++ install packages.R
@@ -1,3 +1,5 @@
+#Data Science Course
+
 install.packages("tidyverse") 
 install.packages("dslabs")
 install.packages("ggthemes")
@@ -7,11 +9,13 @@ install.packages("titanic")
 install.packages("gtools")
 install.packages("markdown")
 
+#FOM
 install.packages("mosaic") # Diesen Befehl bei jeder Session am Anfang ausführen
 install.packages("readr")
 install.packages("gplots")
 install.packages("dunn.test")
 
+#Extras
 install.packages("psych2")
 install.packages("ggplot22")
 install.packages("vcd2")

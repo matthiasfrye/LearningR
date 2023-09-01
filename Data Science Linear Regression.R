4029320db0c5f996690ebd0d9c52944b443a289f
Matthias Frye <matthias_frye@hotmail.com>
2023-09-01 22:38:25 +0200

    initial

---
+++ Data Science Linear Regression.R
@@ -0,0 +1,21 @@
+library(tidyverse) #load also ggplot2
+library(dslabs)
+library(ggthemes)
+library(ggrepel)
+library(gridExtra)
+library(titanic)
+library(gtools)
+library(rvest)
+library(purrr)
+library(lubridate)
+library(tidyr)
+library(scales)
+library(tidytext)
+
+
+library(mosaic) # Diesen Befehl bei jeder Session am Anfang ausführen
+library(readr)
+library(gplots)
+library(readxl)
+
+## Section 1

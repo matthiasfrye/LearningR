948ee0d85fd0a11348e71664b73292cfdda6b3fd
Matthias Frye <matthias_frye@hotmail.com>
2023-10-06 18:10:26 +0200

    progress

--- Data Science Machine Learning.R
+++ Data Science Machine Learning.R
@@ -12,6 +12,7 @@ library(tidyr)
 library(scales)
 library(tidytext)
 library(broom)
+library(caret)
 
 
 library(mosaic) # Diesen Befehl bei jeder Session am Anfang ausführen
@@ -19,3 +20,417 @@ library(readr)
 library(gplots)
 library(readxl)
 
+
+
+## Sex predicted by height
+data(heights)
+
+# define the outcome and predictors
+y <- heights$sex
+x <- heights$height
+
+# generate training and test sets
+set.seed(2007)
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+test_set <- heights[test_index, ]
+train_set <- heights[-test_index, ]
+
+# guess the outcome
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
+
+# compute accuracy
+mean(y_hat == test_set$sex)
+
+# compare heights in males and females in our data set
+heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
+
+# now try predicting "male" if the height is within 2 SD of the average male
+y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+mean(y == y_hat)
+
+# examine the accuracy of 50 cutoffs
+cutoff <- seq(61, 70, 0.2)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+#plot accuracy
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+#find best cutoff
+best_cutoff <- cutoff[which.max(accuracy)]
+best_cutoff
+
+#test cutoff with test data set
+y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
+
+#alternative model using linear regression
+train_set$sex_num <- ifelse(train_set$sex=="Male", 1,0)
+mod<-lm(sex_num~height,data=train_set)
+mod
+
+#if predicted value is biggr than 0.5
+y_hat <- ifelse(predict(mod, data.frame(height=test_set$height))  >= 0.5, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
+
+#exercise
+mnist <- read_mnist()
+str(mnist)
+
+##accuracy
+# tabulate each combination of prediction and actual value
+table(predicted = y_hat, actual = test_set$sex)
+test_set %>% 
+  mutate(y_hat = y_hat) %>%
+  group_by(sex) %>% 
+  summarize(accuracy = mean(y_hat == sex))
+prev <- mean(y == "Male")
+
+confusionMatrix(data = y_hat, reference = test_set$sex)
+
+# get the metrics
+cm <- confusionMatrix(data = y_hat, reference = test_set$sex)
+
+# access specific metrics
+cm$overall["Accuracy"]
+
+cm$byClass[c("Sensitivity","Specificity", "Prevalence")]
+
+
+# maximize F-score
+cutoff <- seq(61, 70)
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+})
+
+data.frame(cutoff, F_1) %>% 
+  ggplot(aes(cutoff, F_1)) + 
+  geom_point() + 
+  geom_line()
+
+max(F_1)
+
+best_cutoff_2 <- cutoff[which.max(F_1)]
+best_cutoff_2
+
+y_hat <- ifelse(test_set$height > best_cutoff_2, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+sensitivity(data = y_hat, reference = test_set$sex)
+specificity(data = y_hat, reference = test_set$sex)
+
+
+#ROC curves
+p <- 0.9
+n <- length(test_index)
+y_hat <- sample(c("Male", "Female"), n, replace = TRUE, prob=c(p, 1-p)) %>% 
+  factor(levels = levels(test_set$sex))
+mean(y_hat == test_set$sex)
+
+# ROC curve
+probs <- seq(0, 1, length.out = 10)
+guessing <- map_df(probs, function(p){
+  y_hat <- 
+    sample(c("Male", "Female"), n, replace = TRUE, prob=c(p, 1-p)) %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Guessing",
+       FPR = 1 - specificity(y_hat, test_set$sex),
+       TPR = sensitivity(y_hat, test_set$sex))
+})
+guessing %>% qplot(FPR, TPR, data =., xlab = "1 - Specificity", ylab = "Sensitivity")
+
+cutoffs <- c(50, seq(60, 75), 80)
+height_cutoff <- map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Height cutoff",
+       FPR = 1-specificity(y_hat, test_set$sex),
+       TPR = sensitivity(y_hat, test_set$sex))
+})
+
+# plot both curves together
+bind_rows(guessing, height_cutoff) %>%
+  ggplot(aes(FPR, TPR, color = method)) +
+  geom_line() +
+  geom_point() +
+  xlab("1 - Specificity") +
+  ylab("Sensitivity")
+
+library(ggrepel)
+map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Height cutoff",
+       cutoff = x, 
+       FPR = 1-specificity(y_hat, test_set$sex),
+       TPR = sensitivity(y_hat, test_set$sex))
+}) %>%
+  ggplot(aes(FPR, TPR, label = cutoff)) +
+  geom_line() +
+  geom_point() +
+  geom_text_repel(nudge_x = 0.01, nudge_y = -0.01)
+
+# plot precision against recall
+guessing <- map_df(probs, function(p){
+  y_hat <- sample(c("Male", "Female"), length(test_index), 
+                  replace = TRUE, prob=c(p, 1-p)) %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Guess",
+       recall = sensitivity(y_hat, test_set$sex),
+       precision = precision(y_hat, test_set$sex))
+})
+
+height_cutoff <- map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Female", "Male"))
+  list(method = "Height cutoff",
+       recall = sensitivity(y_hat, test_set$sex),
+       precision = precision(y_hat, test_set$sex))
+})
+
+bind_rows(guessing, height_cutoff) %>%
+  ggplot(aes(recall, precision, color = method)) +
+  geom_line() +
+  geom_point()
+guessing <- map_df(probs, function(p){
+  y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE, 
+                  prob=c(p, 1-p)) %>% 
+    factor(levels = c("Male", "Female"))
+  list(method = "Guess",
+       recall = sensitivity(y_hat, relevel(test_set$sex, "Male", "Female")),
+       precision = precision(y_hat, relevel(test_set$sex, "Male", "Female")))
+})
+
+height_cutoff <- map_df(cutoffs, function(x){
+  y_hat <- ifelse(test_set$height > x, "Male", "Female") %>% 
+    factor(levels = c("Male", "Female"))
+  list(method = "Height cutoff",
+       recall = sensitivity(y_hat, relevel(test_set$sex, "Male", "Female")),
+       precision = precision(y_hat, relevel(test_set$sex, "Male", "Female")))
+})
+bind_rows(guessing, height_cutoff) %>%
+  ggplot(aes(recall, precision, color = method)) +
+  geom_line() +
+  geom_point()
+
+
+##exercise
+
+library(dslabs)
+library(dplyr)
+library(lubridate)
+data(reported_heights)
+
+dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
+  filter(date_time >= make_date(2016, 01, 25) & date_time < make_date(2016, 02, 1)) %>%
+  mutate(type = ifelse(day(date_time) == 25 & hour(date_time) == 8 & between(minute(date_time), 15, 30), "inclass","online")) %>%
+  select(sex, type)
+
+y <- factor(dat$sex, c("Female", "Male"))
+x <- dat$type
+
+mean(y[x=="inclass"]=="Female")
+mean(y[x=="online"]=="Female")
+
+y_hat <- ifelse(x=="online", "Male", "Female") %>% 
+  factor(levels = levels(y))
+mean(y_hat == y)
+table(y_hat, y)
+sensitivity(data = y_hat, reference = y)
+specificity(data = y_hat, reference = y)
+mean(y=="Female")
+
+
+library(caret)
+data(iris)
+iris <- iris[-which(iris$Species=='setosa'),]
+y <- iris$Species
+
+set.seed(76)
+test_index <- createDataPartition(y, times=1, p=0.5, list=FALSE)
+test <- iris[test_index,]
+train <- iris[-test_index,]
+
+# accuracy of Sepal.length
+mean(train$Sepal.Length~train$Species)
+min(train$Sepal.Length)
+max(train$Sepal.Length)
+
+# examine the accuracy of 50 cutoffs
+cutoff <- seq(4.9, 7.9, 0.1)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train$Sepal.Length > x, "virginica", "versicolor") %>% 
+    factor(levels = levels(train$Species))
+  mean(y_hat == train$Species)
+})
+#plot accuracy
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+# accuracy of Sepal.Width
+mean(train$Sepal.Width~train$Species)
+min(train$Sepal.Width)
+max(train$Sepal.Width)
+
+# examine the accuracy of 50 cutoffs
+cutoff <- seq(2, 3.8, 0.1)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train$Sepal.Width > x, "virginica", "versicolor") %>% 
+    factor(levels = levels(train$Species))
+  mean(y_hat == train$Species)
+})
+#plot accuracy
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+# accuracy of Petal.Length
+mean(train$Petal.Length~train$Species)
+min(train$Petal.Length)
+max(train$Petal.Length)
+
+# examine the accuracy of 50 cutoffs
+cutoff <- seq(3.3, 6.7, 0.1)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train$Petal.Length > x, "virginica", "versicolor") %>% 
+    factor(levels = levels(train$Species))
+  mean(y_hat == train$Species)
+})
+#plot accuracy
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+#find best cutoff
+best_length_cutoff <- cutoff[which.max(accuracy)]
+best_length_cutoff
+
+
+# accuracy of Petal.Width
+mean(train$Petal.Width~train$Species)
+min(train$Petal.Width)
+max(train$Petal.Width)
+
+# examine the accuracy of 50 cutoffs
+cutoff <- seq(1, 2.5, 0.1)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train$Petal.Width > x, "virginica", "versicolor") %>% 
+    factor(levels = levels(train$Species))
+  mean(y_hat == train$Species)
+})
+#plot accuracy
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+#accuracy in test data
+#find best cutoff
+best_width_cutoff <- cutoff[which.max(accuracy)]
+best_width_cutoff
+
+#predict test data with Petal.Width
+y_hat <- ifelse(test$Petal.Width > best_width_cutoff, "virginica", "versicolor") %>% 
+  factor(levels = levels(test$Species))
+mean(y_hat == test$Species)
+
+
+#predict ttest data with Petal.Width and with Petal.Length
+y_hat <- ifelse(test$Petal.Width > best_width_cutoff & test$Petal.Length > best_length_cutoff, "virginica", "versicolor") %>% 
+  factor(levels = levels(test$Species))
+mean(y_hat == test$Species)
+
+
+plot(iris, pch=21, bg=iris$Species)
+
+##Exercise
+
+# Q1
+
+# P( d ) = 0.02
+# P( t+ | d ) = 0.85
+# P( t- | h ) = 0.9    => P( t+ | h ) = 0.1
+# P( t+ ) = P( t+ | d )* P( d ) + P( t+ | h ) *P( h )
+0.85 * 0.02 + 0.1 *0.98
+# = 0.115
+
+
+# P( d | t+ ) = P( t+ | d ) * P( d )/ P( t+ )
+ 0.85 * 0.02 / 0.115
+# = 0.1478261
+
+#Q2-Q4 
+set.seed(1) 
+disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
+test <- rep(NA, 1e6)
+test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90,0.10))
+test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
+
+# Q2
+mean(test) 
+
+# Q3
+# sample data
+mean(disease[test==0])
+
+#theoretical solution
+# P( d | t- ) = P( t- | d ) * P( d ) / P( t- ) 
+(1-0.85) * 0.02 / (1-0.115)
+
+#Q4
+# sample data
+mean(disease[test==1])
+
+#Q5
+
+mean(disease[test==1]) / 0.02
+
+#Q6
+
+library(dslabs)
+data("heights")
+heights %>% 
+  mutate(height = round(height)) %>%
+  group_by(height) %>%
+  summarize(p = mean(sex == "Male")) %>%
+  qplot(height, p, data =.)
+
+#Q7
+ps <- seq(0, 1, 0.1)
+heights %>% 
+  mutate(g = cut(height, quantile(height, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(p = mean(sex == "Male"), height = mean(height)) %>%
+  qplot(height, p, data =.)
+
+#Q8
+Sigma <- 9*matrix(c(1,0.5,0.5,1), 2, 2)
+dat <- MASS::mvrnorm(n = 10000, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+plot(dat)
+
+ps <- seq(0, 1, 0.1)
+dat %>% 
+  mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(y = mean(y), x = mean(x)) %>%
+  qplot(x, y, data =.)

b79ec80e930467b2b1f945d33f8c85281b367f34
Matthias Frye <matthias_frye@hotmail.com>
2023-11-06 20:37:46 +0100

    gam package map packages

--- install packages.R
+++ install packages.R
@@ -17,6 +17,7 @@ install.packages("lazyeval")
 install.packages("textdata")
 install.packages("HistData")
 install.packages("caret")
+install.packages("gam")
 
 #FOM Quantitative Methoden
 install.packages("mosaic") # Diesen Befehl bei jeder Session am Anfang ausführen
@@ -29,4 +30,24 @@ install.packages("dunn.test")
 install.packages("psych2")
 install.packages("ggplot22")
 install.packages("vcd2")
+install.packages("devtools")
+install.packages("formattable")
+
+#Maps
+install.packages("ggmap")
+install.packages("maps")
+install.packages("mapdata")
+install.packages("sf")
+install.packages("sp")
+install.packages("geodata")
+install.packages("rnaturalearth")
+devtools::install_github("ropensci/rnaturalearthhires")
+
+install.packages("elevatr")
+install.packages("terra")
+install.packages("whitebox")
+install.packages("tidyterra")
+install.packages("giscoR")
+install.packages("ggnewscale")
+install.packages("ggblend")
 
